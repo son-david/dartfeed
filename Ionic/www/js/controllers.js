8f498dd0cc25793e1d2e675c66612f79302f3990
son-david <sondavid92@gmail.com>
2015-11-20 17:02:26 -0800

    working on displaying newsfeeds for diff categories

--- Ionic/www/js/controllers.js
+++ Ionic/www/js/controllers.js
@@ -79,6 +79,7 @@ angular.module('starter.controllers', ['ngOpenFB', 'app.services'])
 }])
 
 .controller('UserController', ['$scope', '$state', function($scope, $state) {
+
   $scope.categories = [
     { title: 'MoneyTech', id: 1 },
     { title: 'SportsTech', id: 2 },
