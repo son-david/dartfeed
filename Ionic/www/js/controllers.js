f535a98759704e0b108f4e610106c30a000ec7de
William <william93lee@gmail.com>
2015-11-21 11:03:30 -0800

    Made ngFB in the usercontroller

--- Ionic/www/js/controllers.js
+++ Ionic/www/js/controllers.js
@@ -62,7 +62,7 @@ angular.module('starter.controllers', ['ngOpenFB', 'app.services'])
   };
 }])
 
-.controller('UserController', ['$scope', '$state', function($scope, $state) {
+.controller('UserController', ['$scope', '$state', 'ngFB' function($scope, $state, ngFB) {
   $scope.categories = [
     { title: 'MoneyTech', id: 1 },
     { title: 'SportsTech', id: 2 },
