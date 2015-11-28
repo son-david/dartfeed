b584a3abc00cd5c66b069de73e5ae229d44013ce
son-david <sondavid92@gmail.com>
2015-11-28 15:31:26 -0800

    stripping controllers for login in prep for constructing a get USER request function

--- Ionic/www/js/controllers.js
+++ Ionic/www/js/controllers.js
@@ -30,18 +30,6 @@ angular.module('starter.controllers', ['ngOpenFB', 'app.services'])
     $scope.modal.show();
   };
 
-
-  // Perform the login action when the user submits the login form
-  $scope.doLogin = function() {
-    console.log('Doing login', $scope.loginData);
-
-    // Simulate a login delay. Remove this and replace with your login
-    // code if using a login system
-    $timeout(function() {
-      $scope.closeLogin();
-    }, 1000);
-  };
-
   $scope.fbLogin = function () {
     ngFB.login({scope: 'email, user_friends'}).then(
         function (response) {
